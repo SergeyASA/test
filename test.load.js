49d86c14aeda38df6dd8fce4e969c81349106b1a
SergeyASA <44337027+SergeyASA@users.noreply.github.com>
2018-10-21 14:33:28 +0300

    Update test.load.js

---
+++ test.load.js
@@ -0,0 +1,4 @@
+var custom_js=document.createElement("script");
+custom_js.type="text/javascript";
+custom_js.src="//github.com/SergeyASA/test/raw/master/test.js";
+document.getElementsByTagName('head')[0].appendChild(custom_js)
